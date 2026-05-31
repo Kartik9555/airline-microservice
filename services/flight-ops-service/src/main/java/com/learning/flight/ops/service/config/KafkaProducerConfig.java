df8137a92606bf6e90dd92ca6378983744370bcd
kartik9555 <kartik9555@gmail.com>
2026-05-31 17:39:38 +0200

    Fixed kafka errors and docker image name

--- services/flight-ops-service/src/main/java/com/learning/flight/ops/service/config/KafkaProducerConfig.java
+++ services/flight-ops-service/src/main/java/com/learning/flight/ops/service/config/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.learning.flight.ops.service.config;
 
+import com.learning.common.event.FlightInstanceCreatedEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +21,7 @@ public class KafkaProducerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
+    public ProducerFactory<String, FlightInstanceCreatedEvent> producerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -29,7 +30,7 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
+    public KafkaTemplate<String, FlightInstanceCreatedEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
